b3dbf2f27db9fcd7e1113d16ddbc73da0cb16f82
Nirav Patel <nrvpatel03@gmail.com>
2018-07-11 20:44:05 -0700

    created card, header

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import Header from "./components/Header";
+import hedgehog from "./hedgehog.json";
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    hedgehog,
+    score: 0
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+        <Header
+        title="This is my Header"
+        score={"Current Score " + this.state.score}
+        />
+        {this.state.hedgehog.map(hedge => (
           <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={hedge.id}
+            key={hedge.id}
+            image={hedge.image}
+            name={hedge.name}
           />
         ))}
       </Wrapper>
